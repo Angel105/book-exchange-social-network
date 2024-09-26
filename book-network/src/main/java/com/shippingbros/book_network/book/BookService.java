a72b47ee2a9057a0d4b2b0fa0f1f41237aa34953
aponoma <siteandstock@gmail.com>
2024-09-26 21:03:34 +0200

    [28] implement the save book method [4:15:34]

---
+++ book-network/src/main/java/com/shippingbros/book_network/book/BookService.java
@@ -0,0 +1,21 @@
+package com.shippingbros.book_network.book;
+
+import com.shippingbros.book_network.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookMapper bookMapper;
+    private final BookRepository repository;
+
+    public Integer save(BookRequest request, Authentication connectedUser) {
+        User user = (User) connectedUser.getPrincipal();
+        Book book = bookMapper.toBook(request);
+        book.setOwner(user);
+        return repository.save(book).getId();
+    }
+}

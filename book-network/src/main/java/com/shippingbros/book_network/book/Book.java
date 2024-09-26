2661b2049fd917b4b03cb5a0f34324d0a87beddf
aponoma <siteandstock@gmail.com>
2024-09-26 19:27:17 +0200

    [25] create the BaseEntity for common fields, use inheritance and refactor the code [3:46:44]

--- book-network/src/main/java/com/shippingbros/book_network/book/Book.java
+++ book-network/src/main/java/com/shippingbros/book_network/book/Book.java
@@ -1,20 +1,21 @@
 package com.shippingbros.book_network.book;
 
-import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import com.shippingbros.book_network.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
-
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Book {
+public class Book extends BaseEntity {
 
-    @Id
-    @GeneratedValue
-    private Integer id;
     private String title;
     private String authorName;
     private String isbn;
@@ -22,20 +23,4 @@ public class Book {
     private String bookCover;
     private boolean archived;
     private boolean shareable;
-
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private Integer createdBy;
-
-    @LastModifiedDate
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
 }
--- book-network/src/main/java/com/shippingbros/book_network/feedback/Feedback.java
+++ book-network/src/main/java/com/shippingbros/book_network/feedback/Feedback.java
@@ -1,36 +1,22 @@
 package com.shippingbros.book_network.feedback;
 
-import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import com.shippingbros.book_network.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
-
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Feedback {
+public class Feedback extends BaseEntity {
 
-    @Id
-    @GeneratedValue
-    private Integer id;
     private Double note; // 1-5 stars
     private String comment;
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private Integer createdBy;
-
-    @LastModifiedDate
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
 }

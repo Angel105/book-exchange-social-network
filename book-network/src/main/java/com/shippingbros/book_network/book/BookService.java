39886816653da55e0d7ce553e2034bf7835032f4
aponoma <siteandstock@gmail.com>
2024-09-27 13:23:37 +0200

    [33] implement the find all returned books [4:55:44]

--- book-network/src/main/java/com/shippingbros/book_network/book/BookService.java
+++ book-network/src/main/java/com/shippingbros/book_network/book/BookService.java
@@ -96,4 +96,23 @@ public class BookService {
         );
 
     }
+
+    public PageResponse<BorrowedBookResponse> findAllReturnedBooks(int page, int size, Authentication connectedUser) {
+        User user = (User) connectedUser.getPrincipal();
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
+        Page<BookTransactionHistory> allBorrowedBooks = transactionHistoryRepository.findAllReturnedBooks(pageable, user.getId());
+        List<BorrowedBookResponse> bookResponse = allBorrowedBooks.stream()
+                .map(bookMapper::toBorrowedBookResponse)
+                .toList();
+
+        return new PageResponse<>(
+                bookResponse,
+                allBorrowedBooks.getNumber(),
+                allBorrowedBooks.getSize(),
+                allBorrowedBooks.getTotalElements(),
+                allBorrowedBooks.getTotalPages(),
+                allBorrowedBooks.isFirst(),
+                allBorrowedBooks.isLast()
+        );
+    }
 }

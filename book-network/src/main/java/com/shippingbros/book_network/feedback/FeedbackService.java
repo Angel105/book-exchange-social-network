03b1ab088528d8cf7a603b33109ac2b3b6c158a2
aponoma <siteandstock@gmail.com>
2024-09-28 21:47:26 +0200

    [44] implement sorting of FeedbacksByBook by createdDate in descending order

--- book-network/src/main/java/com/shippingbros/book_network/feedback/FeedbackService.java
+++ book-network/src/main/java/com/shippingbros/book_network/feedback/FeedbackService.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -42,7 +43,7 @@ public class FeedbackService {
     }
 
     public PageResponse<FeedbackResponse> findAllFeedbacksByBook(Integer bookId, int page, int size, Authentication connectedUser) {
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
         User user = (User) connectedUser.getPrincipal();
         Page<Feedback> feedbacks = feedbackRepository.findAllByBookId(bookId, pageable);
         List<FeedbackResponse> feedbackResponses = feedbacks.stream()

8deb8fb8075d3a6f74d8ee5bd28f2bd0d4672fee
aponoma <siteandstock@gmail.com>
2024-09-29 22:13:58 +0200

    [46] implement the Login page, add the CORS filter in the back-end [7:32:29]

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -2,20 +2,23 @@ import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {CommonModule} from "@angular/common";
-import {RouterOutlet} from "@angular/router";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "./app-routing.module";
+import {LoginComponent} from "./pages/login/login.component";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
     CommonModule,
-    RouterOutlet,
-    // AppRoutingModule,
-    HttpClientModule
+    FormsModule
   ],
   providers: [
     HttpClient

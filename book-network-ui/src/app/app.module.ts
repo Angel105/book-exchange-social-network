d2eeeb3e6f600ad199a1d1002baabbc1c50dcfab
aponoma <siteandstock@gmail.com>
2024-10-10 16:31:25 +0200

    [52] inject the jwt Bearer token that we send with each our request to our back-end using an HTTP interceptor (HttpTokenInterceptor is registered in app.modules.ts in providers) [8:43:22]

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {CommonModule} from "@angular/common";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from "./app-routing.module";
 import {LoginComponent} from "./pages/login/login.component";
 import {FormsModule} from "@angular/forms";
 import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 import {CodeInputModule} from "angular-code-input";
+import {HttpTokenInterceptor} from "./services/interceptor/http-token.interceptor";
 
 
 @NgModule({
@@ -27,7 +28,12 @@ import {CodeInputModule} from "angular-code-input";
     CodeInputModule
   ],
   providers: [
-    HttpClient
+    HttpClient,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpTokenInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })

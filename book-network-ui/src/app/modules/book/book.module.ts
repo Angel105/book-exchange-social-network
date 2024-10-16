b7e0ac75e08d837315becc109f4305884e9cda07
aponoma <siteandstock@gmail.com>
2024-10-16 22:00:44 +0200

    [62] implement the return borrowed book page [11:04:32]

--- book-network-ui/src/app/modules/book/book.module.ts
+++ book-network-ui/src/app/modules/book/book.module.ts
@@ -10,6 +10,7 @@ import { RatingComponent } from './components/rating/rating.component';
 import { MyBooksComponent } from './pages/my-books/my-books.component';
 import { BookManagementComponent } from './pages/book-management/book-management.component';
 import {FormsModule} from "@angular/forms";
+import { BorrowedBookListComponent } from './pages/borrowed-book-list/borrowed-book-list.component';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import {FormsModule} from "@angular/forms";
     BookCardComponent,
     RatingComponent,
     MyBooksComponent,
-    BookManagementComponent
+    BookManagementComponent,
+    BorrowedBookListComponent
   ],
   imports: [
     CommonModule,
